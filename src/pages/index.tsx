1eb8a233d9d1be09459480db1ad59d91e1f24cf3
Adam Hartman <adamhartman5@gmail.com>
2023-02-24 12:26:25 -0500

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import { NextPage } from 'next'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const Home: NextPage = () => {
+  return <div>Hello</div>
+}
+
+export default Home;
