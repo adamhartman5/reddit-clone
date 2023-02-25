2478e1cfea74539acc98b863cb1482f613e0c243
Adam Hartman <adamhartman5@gmail.com>
2023-02-25 15:03:46 -0500

    add images needed and add reddit logo to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,15 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 const Navbar:React.FC = () => {
 	return (
-		<Flex bg="white" border='1px solid red'>
-			Navbar
+		<Flex bg="white" height='44px' padding='6px 12px'>
+			<Flex align='center'>
+				<Image src='/images/redditFace.svg' height='30px' />
+				<Image src='/images/redditText.svg' height='46px' display={{base: 'none', md:'unset'}}/>
+			</Flex>
+			{/*<Directory />
+			<SearchInput />
+			<RightContent />*/}
 		</Flex>
 	)
 }
