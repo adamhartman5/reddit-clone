9a3dcdb42333b0b194331432dcd7d6b12c6c7222
Adam Hartman <adamhartman5@gmail.com>
2023-02-26 02:25:18 -0500

    Finish login and signup

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,3 +1,4 @@
+import { authModalState } from '@/src/atoms/authModalAtom';
 import {
 	useDisclosure,
 	Button,
@@ -8,28 +9,55 @@ import {
 	ModalCloseButton,
 	ModalBody,
 	ModalFooter,
+	Flex,
+	Text,
 } from '@chakra-ui/react';
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import AuthInputs from './AuthInputs';
+import OAuthButtons from './OAuthButtons';
 
 const AuthModal: React.FC = () => {
-	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [modalState, setModalState] = useRecoilState(authModalState);
+
+	const handleClose = () => {
+		setModalState((prev) => ({
+			...prev,
+			open: false,
+		}));
+	};
 	return (
 		<>
-			<Button onClick={onOpen}>Open Modal</Button>
-
-			<Modal isOpen={isOpen} onClose={onClose}>
+			<Modal isOpen={modalState.open} onClose={handleClose}>
 				<ModalOverlay />
 				<ModalContent>
-					<ModalHeader>Modal Title</ModalHeader>
+					<ModalHeader textAlign='center'>
+						{modalState.view === 'login' && 'Login'}
+						{modalState.view === 'signup' && 'Sign Up'}
+						{modalState.view === 'resetPassword' && 'Reset Password'}
+					</ModalHeader>
 					<ModalCloseButton />
-					<ModalBody>Here is the modal body</ModalBody>
-
-					<ModalFooter>
-						<Button colorScheme='blue' mr={3} onClick={onClose}>
-							Close
-						</Button>
-						<Button variant='ghost'>Secondary Action</Button>
-					</ModalFooter>
+					<ModalBody
+						display='flex'
+						flexDirection='column'
+						alignItems='center'
+						justifyContent='center'
+						pb={6}
+					>
+						<Flex
+							direction='column'
+							align='center'
+							justify='center'
+							width='70%'
+						>
+							<OAuthButtons />
+							<Text color='gray.500' fontWeight={700}>
+								OR
+							</Text>
+							<AuthInputs />
+							{/* <ResetPassword /> */}
+						</Flex>
+					</ModalBody>
 				</ModalContent>
 			</Modal>
 		</>
