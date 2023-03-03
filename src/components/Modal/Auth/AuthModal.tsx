c8c0d1546b4e476537a6ed6992f73daa20ed2acf
Adam Hartman <adamhartman5@gmail.com>
2023-03-03 00:06:59 -0500

    Add logged in user state and logout func

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,4 +1,5 @@
 import { authModalState } from '@/src/atoms/authModalAtom';
+import { auth } from '@/src/firebase/clientApp';
 import {
 	useDisclosure,
 	Button,
@@ -12,13 +13,15 @@ import {
 	Flex,
 	Text,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilState } from 'recoil';
 import AuthInputs from './AuthInputs';
 import OAuthButtons from './OAuthButtons';
 
 const AuthModal: React.FC = () => {
 	const [modalState, setModalState] = useRecoilState(authModalState);
+	const [user, loading, error] = useAuthState(auth);
 
 	const handleClose = () => {
 		setModalState((prev) => ({
@@ -26,6 +29,11 @@ const AuthModal: React.FC = () => {
 			open: false,
 		}));
 	};
+
+	useEffect(() => {
+		if (user) handleClose();
+	}, [user]);
+
 	return (
 		<>
 			<Modal isOpen={modalState.open} onClose={handleClose}>
