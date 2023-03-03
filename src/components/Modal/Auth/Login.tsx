98e2a1b8d4daa6bce16769baded910b1dc3fce93
Adam Hartman <adamhartman5@gmail.com>
2023-03-02 23:12:19 -0500

    Add login functionality

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -2,6 +2,10 @@ import { authModalState } from '@/src/atoms/authModalAtom';
 import { Button, Flex, Input, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '@/src/firebase/clientApp';
+import { FIREBASE_ERRORS } from '@/src/firebase/errors';
 
 type LoginProps = {};
 
@@ -11,9 +15,14 @@ const Login: React.FC<LoginProps> = () => {
 		email: '',
 		password: '',
 	});
+	const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
 
 	// Firebase logic
-	const onSubmit = () => {};
+	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		signInWithEmailAndPassword(loginForm.email, loginForm.password);
+	};
 
 	const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setLoginForm((prev) => ({
@@ -55,6 +64,7 @@ const Login: React.FC<LoginProps> = () => {
 				}}
 				bg='gray.50'
 			/>
+			<Text textAlign='center' color='red' fontSize='10pt'>{FIREBASE_ERRORS[error?.message as keyof typeof FIREBASE_ERRORS]}</Text>
 			<Button width='100%' height='36px' mt={2} mb={2} type='submit'>
 				Log In
 			</Button>
